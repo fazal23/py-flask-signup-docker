f848fff0274b2709a3c3f5c38f2f767086f1691c
jicomusic <jicowan@amazon.com>
2016-08-28 16:21:02 -0500

    Updated test_application.py
    Changed line 12 to the URL of the ALB for the flask-signup service

--- jenkins-slave/test_application.py
+++ jenkins-slave/test_application.py
@@ -9,7 +9,7 @@ class TestSignup(unittest.TestCase):
         self.driver.set_window_size(1120, 550)
 
     def test_signup(self):
-        self.driver.get("http://elb-flask-signup-beta-7177155.us-east-1.elb.amazonaws.com/#signupModal")
+        self.driver.get("http://flask-signup-alb-1516997298.us-east-1.elb.amazonaws.com/#signupModal")
 
     def tearDown(self):
         self.driver.quit()
